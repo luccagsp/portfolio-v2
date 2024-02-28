4600de8bfe9702f809e745bebaf917126914cd39
Lucca <131675134+luccagsp@users.noreply.github.com>
2024-02-28 17:28:44 -0300

    tailwind changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			height: {
+        screen: ['100vh /* fallback for Opera, IE and etc. */', '100dvh'],
+      },
+			width: {
+        screen: ['100vw /* fallback for Opera, IE and etc. */', '100dvw'],
+      }
+		},
 	},
 	plugins: [],
 }
